016b9f083c67854b16781fdcb6fc18ec1c2ad996
kondurj <jayasekhar.konduru@cdk.com>
2017-09-10 15:19:44 +0530

    Initial changes to start embedded server

---
+++ boot/sample/src/main/java/com/jk/spring/boot/sample/DefaultContextProcessor.java
@@ -0,0 +1,19 @@
+package com.jk.spring.boot.sample;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@EnableAutoConfiguration
+public class DefaultContextProcessor {
+
+    @RequestMapping("/")
+    String defaultContext() {
+        return "Welcome to Spring WORLD!!!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(DefaultContextProcessor.class, args);
+    }
+}
---
+++ boot/sample/src/test/java/com/jk/spring/boot/sample/DefaultContextProcessorTests.java
@@ -0,0 +1,16 @@
+package com.jk.spring.boot.sample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class DefaultContextProcessorTests {
+
+	@Test
+	public void contextLoads() {
+		// nothing for now
+	}
+
+}
